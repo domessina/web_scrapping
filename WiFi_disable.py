1b8e42973631bb7c1036b59e867c774909ac5b92
MessinaCorsair <dmessina@outlook.be>
2022-05-21 15:06:19 +0200

    initial commit

---
+++ WiFi_disable.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+import hashlib
+import sys
+import requests
+import re
+
+print('script started')
+
+router_address = 'http://192.168.0.1'
+username = 'voo'
+password = 'Mthwftko1'
+headers = {'User-Agent': 'Mozilla/5.0', 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+           'X-CSRF-TOKEN': '', 'X-Requested-With': 'XMLHttpRequest', 'Referer': 'http://192.168.0.1/'}
+session = requests.Session() # pour garder les cookies set par les reponses routeur
+
+# LOGIN
+salt_response = session.post(router_address+'/api/v1/session/login', headers=headers, data={'username': username, 'password': 'seeksalthash'}).json()
+if (salt_response['error'] != 'ok'):
+    print('{}');
+    sys.exit(0)
+
+# doPbkdf2NotCoded(DoPbkdf2NotCoded(password, salt), saltWebUI)
+a = hashlib.pbkdf2_hmac('sha256', bytes(password, 'utf-8'), bytes(salt_response['salt'], 'utf-8'), 1000).hex()[:32]
+b = hashlib.pbkdf2_hmac('sha256', bytes(a, 'utf-8'), bytes(salt_response['saltwebui'], 'utf-8'), 1000).hex()[:32]
+
+response = session.post(router_address+'/api/v1/session/login', headers=headers, data={'username': 'voo', 'password': b})
+#print(response.json())
+token = re.search("[a-f0-9]{32}", response.headers['Set-Cookie']).group()
+headers['X-CSRF-TOKEN'] = token
+
+# pour set les cookies dans les headers et autres, necessaire
+response = session.get(router_address+'/api/v1/session/menu', headers=headers)
+
+# WIFI ENABLE
+session.post(router_address+'/api/v1/wifi/1,WifiEnable', headers=headers, data={
+    '1[SSID]': 'MayThe4thBeWithYou',
+    '1[SSIDEnable]': 'false',
+    '1[SSIDAdvertisementEnabled]': 'true',
+    '1[ModeEnabled]': 'WPA2-Personal',
+    '1[EncryptionMethod]': 'AES',
+    '1[KeyPassphrase]': 'Mexico2011',
+    '1[WPSEnable]': 'false',
+    'WifiEnable': 'false',
+})
+
+print('ok')
---
+++ readme_vim.txt
@@ -0,0 +1,4 @@
+ne pas laisser de commentaires avec des caractères accentués dans le code
+sites-packages c'est comme node_modules, il y en a un pour tout le systeme et un par projet
+
+solution originelle https://gist.github.com/tiberiucorbu/a51c81b82b5196ac002c52ac6f39987f
